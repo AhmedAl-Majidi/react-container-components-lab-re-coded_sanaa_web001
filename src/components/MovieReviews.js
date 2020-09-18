7be6812d97805aac9409c957189708c60baa6808
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-18 07:00:08 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieReviews = ({reviews}) => {
     return (
         <div className="review-list">
-            {reviews.map( ele => {ele} ) }
+            {reviews.map( ele => ele ) }
         </div>
     )
 }
