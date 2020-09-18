266a2084ee70a9c75d776361fb14b30f77ad1e34
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-18 06:52:24 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+    return (
+        <div>
+            {props.reviews.map( ele =>  <p>{ele}</p>)}
+        </div>
+    )
+}
+
+export default MovieReviews
